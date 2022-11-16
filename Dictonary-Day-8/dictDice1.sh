867dc584327e968cf802541d9f91187b2d596397
maheshnehere <maheshnehere93@gmail.com>
2022-11-16 20:55:37 +0530

    Add files via upload

---
+++ Dictonary-Day-8/dictDice1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+read -p "Enter the number of times the dice is to be rolled: " n
+
+for(( i=0; i<n; i++ ))
+do
+
+    randomCheck=$((RANDOM%6+1))
+
+    echo $randomCheck
+
+    dict[i]=$randomCheck
+
+done
+
+echo ${dict[@]}
